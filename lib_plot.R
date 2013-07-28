d91e1ef55ef3a368ccddb15c6c89175ab6756c55
Jean-Olivier Irisson <irisson@normalesup.org>
2013-07-29 00:30:10 +0200

    Cleanup interpolation functions and add comments

--- lib_plot.R
+++ lib_plot.R
@@ -1,37 +1,52 @@
+#
+#      Useful data representation functions
+#
+#  (c) Copyright 2013 Jean-Olivier Irisson
+#      GNU General Public License v3
+#
+#--------------------------------------------------------------------------
+
+# Spectral colour map from ColorBrewer
 spectral <- function(n=6) {
   library("RColorBrewer")
   rev(brewer.pal(name="Spectral", n=n))
 }
 
-interp.slice <- function(x, y, z, nx=length(x)/30, ny=max(y), ...) {
+# Interpolate a slice of data for which the x-axis is time
+interp.time <- function(x, y, z, nx=length(x)/100, y.step=2.5, ...) {
   library("akima")
   library("reshape2")
 
-  timePOSIX <- time
+  # interpolate as numbers
   time <- as.numeric(time)
-  i <- interp(x=time, y=y, z=z, xo=seq(min(time), max(time), length=nx), yo=seq(min(y), max(y), length=ny), ...)
-	out <- melt(i$z, varnames=c("time","y"))
-	out$time <- i$x[out$time]
-	out$y <- i$y[out$y]
-	out <- rename(out, c(value="z"))
-  
+  i <- interp(x=time, y=y, z=z, xo=seq(min(time), max(time), length=nx), yo=seq(min(y), max(y), by=y.step), ...)
+
+  # extract a data.frame
+  out <- melt(i$z, varnames=c("time","y"))
+  out$time <- i$x[out$time]
+  out$y <- i$y[out$y]
+
+  # reconvert to time
   out$time <- as.POSIXct(out$time, origin=as.POSIXct("1970-01-01 01:00:00.00"))
-  
-  out <- na.omit(out)
-  
+
   return(out)
 }
 
+# Interpolate a slice of data for which the x-axis is a distance in nautical miles
 interp.dist <- function(x, y, z, anisotropy=1000, x.step=500, y.step=2.5, ...) {
   library("akima")
   library("reshape2")
 
-  x <- x * 1852 / anisotropy 
+  # correct x-axis for anisotropy between horizontal and vertical
+  x <- x * 1852 / anisotropy
+
+  # interpolate
   i <- interp(x=x, y=y, z=z, xo=seq(0, max(x), by=x.step/anisotropy), yo=seq(0, max(y), by=y.step), ...)
-  
-	out <- melt(i$z, varnames=c("x","y"))
-	out$x <- i$x[out$x] * anisotropy / 1852
-	out$y <- i$y[out$y]
-  
+
+  # extract a data.frame
+  out <- melt(i$z, varnames=c("x","y"))
+  out$x <- i$x[out$x] * anisotropy / 1852
+  out$y <- i$y[out$y]
+
   return(out)
 }
