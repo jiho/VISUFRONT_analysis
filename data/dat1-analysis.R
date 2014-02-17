3a9c44d44dda3d4f35c00a2d7e9b20bba91aee86
Robin Faillettaz <robin.faillettaz@gmail.com>
2014-02-17 23:03:48 +0100

    Clean code

--- data/dat1-analysis.R
+++ data/dat1-analysis.R
@@ -17,7 +17,6 @@ dir <- "/Users/faillettaz/Dropbox/robin/visufront-data/"
 
 library("plyr")
 library("ggplot2")
-source("~/r-utils/lib_zooprocess.R")
 library("stringr")
 library("reshape2")
 library("grid")
@@ -36,23 +35,23 @@ pid <- read.pid(files[2])
 
 pids <- NA
 pids <- adply(files, 1, function(files)
-    {
-    pid <- read.pid(files)
-    # make validation column name uniform
-    names <- names(pid)
-    #names <- str_replace_all(names, "Valid", "Valid")
-    # force latest validation to have the name "Valid"
-    validationColumns <- which(str_detect(names, "Valid"))
-    if (length(validationColumns) == 1) {
-         names[validationColumns] <- "Valid"
-    }
-    if (length(validationColumns) > 1) {
-        names[max(validationColumns)] <- "Valid"    
-    }
-   
-    names(pid) <- names
-    rbind(pids, pid)
-    }, .progress="text")  
+{
+pid <- read.pid(files)
+# make validation column name uniform
+names <- names(pid)
+#names <- str_replace_all(names, "Valid", "Valid")
+# force latest validation to have the name "Valid"
+validationColumns <- which(str_detect(names, "Valid"))
+if (length(validationColumns) == 1) {
+    names[validationColumns] <- "Valid"
+}
+if (length(validationColumns) > 1) {
+    names[max(validationColumns)] <- "Valid"    
+}
+
+names(pid) <- names
+rbind(pids, pid)
+}, .progress="text")  
 pids <- pids[-1, -1]
 
 head(pids)
@@ -61,15 +60,18 @@ head(pids)
 pids$DepthBin <- round_any(pids$Depth, 1)
 
 # get raw abundance
-d <- ddply(pids, ~Valid+Label+DepthBin, function(x) {sum(na.omit(x$Valid==paste(x$Valid)))})
+bio <- ddply(pids, ~Valid+Label+DepthBin, function(x) {sum(na.omit(x$Valid==paste(x$Valid)))})
 
-d <- rename(d, c("V1" = "Abund"))
+bio <- rename(bio, c("V1" = "Abund"))
 
 # plot everything
-ggplot(d) + 
-    geom_point(aes(x = Abund, y = -DepthBin, colour = Label)) + 
-    geom_path(aes(x = Abund, y = -DepthBin, colour = Label)) + 
-    facet_grid(.~Valid, scales="free_x")
+ggplot(bio) + 
+geom_point(aes(x = Abund, y = -DepthBin, colour = Label)) + 
+geom_path(aes(x = Abund, y = -DepthBin, colour = Label)) + 
+facet_grid(.~Valid, scales="free_x")
+
+
+
 ###         Process Physical Data
 ### ---------------------------------------------------------
 
