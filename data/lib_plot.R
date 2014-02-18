834b6c10a646462bcb48d09337bb332f846aeed1
Robin Faillettaz <robin.faillettaz@gmail.com>
2014-02-18 11:12:53 +0100

    Added an interp.smooth function to smooth after a first coarser interpolation

---
+++ data/lib_plot.R
@@ -0,0 +1,69 @@
+#
+#      Useful data representation functions
+#
+#  (c) Copyright 2013 Jean-Olivier Irisson
+#      GNU General Public License v3
+#
+#--------------------------------------------------------------------------
+
+# Spectral colour map from ColorBrewer
+spectral <- function(n=6) {
+  library("RColorBrewer")
+  rev(brewer.pal(name="Spectral", n=n))
+}
+
+# Interpolate a slice of data for which the x-axis is time
+interp.time <- function(x, y, z, nx=length(x)/100, y.step=2.5, ...) {
+  library("akima")
+  library("reshape2")
+
+  # interpolate as numbers
+  time <- as.numeric(time)
+  i <- interp(x=time, y=y, z=z, xo=seq(min(time), max(time), length=nx), yo=seq(min(y), max(y), by=y.step), ...)
+
+  # extract a data.frame
+  out <- melt(i$z, varnames=c("time","y"))
+  out$time <- i$x[out$time]
+  out$y <- i$y[out$y]
+
+  # reconvert to time
+  out$time <- as.POSIXct(out$time, origin=as.POSIXct("1970-01-01 01:00:00.00"))
+
+  return(out)
+}
+
+# Interpolate a slice of data for which the x-axis is a distance in nautical miles
+interp.dist <- function(x, y, z, anisotropy=1000, x.step=500, y.step=2.5, ...) {
+  library("akima")
+  library("reshape2")
+
+  # correct x-axis for anisotropy between horizontal and vertical
+  x <- x * 1852 / anisotropy
+
+  # interpolate
+  i <- interp(x=x, y=y, z=z, xo=seq(0, max(x), by=x.step/anisotropy), yo=seq(0, max(y), by=y.step), ...)
+
+  # extract a data.frame
+    out <- melt(i$z, varnames=c("x","y"))
+    out$x <- i$x[out$x] * anisotropy / 1852
+    out$y <- i$y[out$y]
+
+  return(out)
+}
+
+
+# Interpolate a slice of data for which the x-axis is a distance in nautical miles
+interp.smooth <- function(x, y, z, x.step=1, y.step=1, linear = T, ...) {
+  library("akima")
+  library("reshape2")
+
+  # interpolate
+  i <- interp(x=x, y=y, z=z, xo=seq(0, max(x), by=x.step), yo=seq(0, max(y), by=y.step),  linear = linear, duplicate = "mean", ...)
+    
+  # extract a data.frame
+  out <- melt(i$z, varnames=c("x","y"))
+  out$x <- i$x[out$x]
+  out$y <- i$y[out$y]
+
+  return(out)
+}
