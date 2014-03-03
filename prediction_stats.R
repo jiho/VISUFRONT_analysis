a705225b4985170e6002f683ac5dd0800b575ad7
Jean-Olivier Irisson <irisson@normalesup.org>
2014-03-03 14:24:01 +0100

    Add code from Agathe

--- prediction_stats.R
+++ prediction_stats.R
@@ -5,13 +5,28 @@ source("lib_zooprocess.R")  # for read.pid
 source("lib_confusion.R")   # for confusion_matrix, confusion_stats, etc.
 
 # prefix for the data location
-data <- "~/Dropbox/visufront-data/"
+data <- "C:/Users/petitesorciere/Dropbox/visufront-data/"
 
 # read data
-d <- read.pid(str_c(data, "zooprocess/transect_cc4_01_dat1.txt"))
+# combine info from several transects
+# read data with the same categories predicted
+d01 <- read.pid(str_c(data, "zooprocess/transect_cc4_01_dat1.txt"))
+d03 <- read.pid(str_c(data, "zooprocess/transect_cc4_03_dat1.txt"))
+d05 <- read.pid(str_c(data, "zooprocess/transect_cc4_05_dat1.txt"))
+d17 <- read.pid(str_c(data, "zooprocess/transect_cc4_17_dat1.txt"))
+d25 <- read.pid(str_c(data, "zooprocess/transect_cc4_25_dat1.txt"))
+
+d <- rbind(d01,d03,d05,d17,d25)
+
+
+# reduce to categories of the learning set
+lg <- read.csv(str_c(data, "/zooprocess/learning_groups.csv"))
+d$ValidReduced <- lg$prediction[match(d$Valid, lg$validation)]
+
 
 # compute confusion matrix
-cm <- confusion_matrix(d$Pred, d$Valid)
+cm <- confusion_matrix(d$Pred, d$ValidReduced)
+
 
 # plot confusion matrix
 autoplot(cm)
@@ -23,17 +38,3 @@ confusion_stats(cm)
 confusion_stats(cm, sort.by="recall")
 confusion_stats(cm, sort.by="precision")
 confusion_stats(cm, sort.by="F1")
-
-# reduce to categories of the learning set
-lg <- read.csv(str_c(data, "/zooprocess/learning_groups.csv")
-d$ValidReduced <- lg$prediction[match(d$Valid, lg$validation)]
-
-cm <- confusion_matrix(d$Pred, d$ValidReduced)
-autoplot(cm)
-autoplot(cm, "row")
-autoplot(cm, "col")
-confusion_stats(cm)
-
-# combine info from several transects
-# rbind()
-
--- relationship_env_abund-agathe.R
+++ relationship_env_abund-agathe.R
@@ -1,1 +1,37 @@
-# Toto
+
+library("stringr") # use for str
+library("FactoMineR") # use for PCA
+source("lib_zooprocess.R")
+source("lib_confusion.R")
+
+# read data
+data <- "C:/Users/petitesorciere/Dropbox/visufront-data/"
+isiis<-read.table("isiis.csv",header=TRUE,sep=",",dec=".")
+
+# PCA, variables factor map
+pcaisiis<-PCA(isiis[,2:8])
+round(pcaisiis$eig,2)
+
+# what explain axes 
+barplot(pcaisiis$eig[,1])
+lapply(dimdesc(pcaisiis),lapply,round,2)
+
+dist<-dist(isiis[,2:8],method="euclidean") # memory.size problem
+
+# relationship Depth-abundance
+# read data
+library("plyr")
+d01 <- read.pid(str_c(data, "zooprocess/transect_cc4_01_dat1.txt"))
+d03 <- read.pid(str_c(data, "zooprocess/transect_cc4_03_dat1.txt"))
+d05 <- read.pid(str_c(data, "zooprocess/transect_cc4_05_dat1.txt"))
+d17 <- read.pid(str_c(data, "zooprocess/transect_cc4_17_dat1.txt"))
+d25 <- read.pid(str_c(data, "zooprocess/transect_cc4_25_dat1.txt"))
+
+d <- rbind(d01,d03,d05,d17,d25)
+
+d$DepthRound <- round_any(d$Depth, 2)
+# Plot Valid=f(Depth)
+ggplot(data = d)  + geom_point(aes(x= Valid , y= -Depth), alpha = 0.01)
+ggplot(data = d)  + geom_boxplot(aes(x= Valid , y= -Depth))
+ggplot(data = d)  + geom_violin(aes(x= Valid , y= -Depth))
+
