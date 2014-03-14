7bf13e4c15c87139de47e4f3f7a67892cbb333e9
Robin Faillettaz <robin.faillettaz@gmail.com>
2014-03-14 10:45:32 +0100

    Cleaned and corrected PCA with anomalies only and all variables. The first axis is the one that highlights the front the best

--- spatial-pca-isiis.R
+++ spatial-pca-isiis.R
@@ -12,7 +12,6 @@
 dir <- "/Users/faillettaz/Dropbox/visufront-data/"
 
 
-
 library("plyr")
 library("ggplot2")
 library("stringr")
@@ -22,10 +21,11 @@ library("gridExtra")
 library("akima")
 library("fields")
 library("oce")   
+library("FactoMineR")
 
 
 source("lib_zooprocess.R")
-source("lib_plot_rf.R")
+source("lib_plot.R")
 source("lib_process.R")
 
 
@@ -45,7 +45,7 @@ phy <- phy[-1, ]
 phy <- rename(phy, c("Temp.C" = "Temp.celsius"))
 
 # select only data from upcasts --> Keep first down cast below 25m to improve interpolation
-phy25mUp <- phy[which(phy$down.up %in% "up" | phy$down.up %in% "down" & phy$Depth.m > 28), ]
+phy25mUp <- phy[which(phy$down.up %in% "up" | phy$down.up %in% "down" & phy$Depth.m > 29), ]
 
 
 # check if seems ok
@@ -82,37 +82,26 @@ do.call(grid.arrange, c(plots,list(ncol=1)))
 
 
 
-
-
 #-------------------------------------------------------------
 #       COMPUTE SPATIAL PCA ON PHYSICAL DATA
 #-------------------------------------------------------------
 
 
-
 #   First for raw data 
 #-----------------------------
 
-# cumpute PCA and extract the 2nd axis (variance)
-library(FactoMineR)
-
+# Prepare data to compute PCA 
 d <- dcast(di, Depth.m + distance ~ variable, value.var = "value")
-
 ACP <- na.exclude(d)
 ACP <- ACP[order(ACP$distance),]
-#acp[which(is.na(acp$temperature)),]
 
+# Compute PCA
 res <- PCA(X=ACP[,-c(1,2)], graph=T)
 
-length(res$ind$coord[,2])
 
-# only for one axe
+# plot it per axis
 ACP$pca <- res$ind$coord[,2]
-#colnames(acpM) <- rename(x=colnames(acpM), c(value="ipressure", variable="distance"))
-
-# geom_tile avec geom_contour par dessus
 ggplot(data=ACP, aes(x=distance, y=-Depth.m)) + geom_raster(aes(fill=pca)) + geom_contour(aes(z=pca), colour="white") + scale_fill_gradientn("PCA", colours = spectral()) + scale_x_continuous(name="Distance", expand=c(0,0)) + scale_y_continuous(name="Depth", expand=c(0,0))
-
 
 
 #      Now on anomalies
@@ -130,14 +119,14 @@ dAvg <- ddply(na.omit(d), ~Depth.m, function(x){
 
 
 # Get anomalies of each variables along the transect per vertical profile
-dN <- na.exclude(ddply(d[which(d$distance < 30), ], ~distance, function(x){
+dN <- na.exclude(ddply(d[which(d$distance < 28), ], ~distance, function(x){
     data.frame(
         depth = x$Depth.m[-1],
-        sal = x$Salinity.PPT[-1],# - dAvg$sal,
+        sal = x$Salinity.PPT[-1] - dAvg$sal,
         temp = x$Temp.celsius[-1] - dAvg$temp, 
-        fluo = x$Fluoro.volts[-1],# - dAvg$fluo,
+        fluo = x$Fluoro.volts[-1] - dAvg$fluo,
         oxy = x$Oxygen.ml.l[-1] - dAvg$oxy)
-})) # all variables aren't variables anomalies here, just to try
+}))
 
 
 #   Check if ok 
@@ -160,7 +149,18 @@ do.call(grid.arrange, c(plots,list(ncol=1)))
 
 # compute the PCA with variables anomalies
 pca <- dN[order(dN$distance),]
-res <- PCA(X=pca[,-c(1,2)], graph=T)
+res <- PCA(X=pca[,-c(1,2)], graph=F)
+
+
+# Set a factor variable to identified the quality of the pca to differentiate the areas (coastal, frontal and central)
+pca$position <- NA
+pca$position[which(pca$distance < 14)] <- "coastal"
+pca$position[which(pca$distance > 21)] <- "central"
+pca$position[which(is.na(pca$position))] <- "frontal"
+
+
+# call the acp_plot function to plot the pca
+plot_acp(x = res, colour = pca$position)
 
 
 # save all different possibilites
@@ -168,16 +168,10 @@ pca$pca1 <- res$ind$coord[,1] # ~41%
 pca$pca2 <- res$ind$coord[,2] # ~27%
 pca$pca3 <- res$ind$coord[,3] # ~11%
 pca$pca4 <- res$ind$coord[,4] # ~9%
-pca$pca12 <- res$ind$coord[,1] + res$ind$coord[,2]
-pca$pca13 <- res$ind$coord[,1] + res$ind$coord[,2] + res$ind$coord[,3]
-pca$pca14 <- res$ind$coord[,1] + res$ind$coord[,2] + res$ind$coord[,3] + res$ind$coord[,2]
 
 
-ggplot(data=pca, aes(x=distance, y=-depth)) + geom_raster(aes(fill=pca14)) + geom_contour(aes(z=pca14), colour="white", bins=5) + scale_fill_gradientn("PCA", colours = spectral()) + scale_x_continuous(name="Distance", expand=c(0,0)) + scale_y_continuous(name="Depth", expand=c(0,0))
-
-
-# plot all possible representation of the spatial pca
-allPCA <- melt(pca, id.vars = c("depth", "distance"), measure.vars = grep("pca", names(dN), value=TRUE))
+# plot the output of the pca for each axis
+allPCA <- melt(pca[, !names(pca) %in% c("sal", "temp")], id.vars = c("depth", "distance", "position"), measure.vars = grep("pca", names(pca), value=TRUE))
 plots <- dlply(allPCA, ~variable, function(x) {
         ggplot(x, aes(x=distance, y=depth)) +
         #geom_point(aes(fill=value), shape=21, colour=NA, na.rm=T) +
@@ -194,31 +188,24 @@ do.call(grid.arrange, c(plots,list(ncol=2)))
 
 
 
-
 # Second interpolation over a finer grid for smoothing
 #-----------------------------------------------------------
 
 
-# if updates in the lib_plot
-#source("data/lib_plot.R")
-
-
-#   For one variable only
+#   For axis 1 only
 #-----------------------------
 
-# delete NAs from the previous interpolation
-i2 <- data.frame(distance = dN$distance, Depth.m = dN$depth, value = ACP$pca)
-
-# Second interpolation for all variables
+# create a new df and interpolate smooth from it
+i2 <- data.frame(distance = dN$distance, Depth.m = dN$depth, value = pca$pca1)
 di2 <- interp.smooth(x=i2$distance, y=i2$Depth.m, z=i2$value, x.step = 0.1, y.step = 0.1)
-
 di2 <- rename(di2, c("x"="distance", "y"="Depth.m"))
 
-
-ggplot(di2, aes(x=distance, y=Depth.m)) + geom_raster(aes(fill=value, na.rm=T)) + stat_contour(aes(z=value), colour="white", bins=4, na.rm=TRUE) + scale_fill_gradientn(paste(x$variable), colours=spectral(), na.value=NA) + scale_x_continuous("Distance from shore", expand=c(0,0)) + scale_y_reverse("Depth (m)", expand=c(0,0)) +
+# plot the smoothed map with value of first axis of the pca
+ggplot(di2, aes(x=distance, y=Depth.m)) + geom_raster(aes(fill=value, na.rm=T)) + stat_contour(aes(z=value), colour="white", bins=5, na.rm=TRUE) + scale_fill_gradientn("PCA1", colours=spectral(), na.value=NA) + scale_x_continuous("Distance from shore", expand=c(0,0)) + scale_y_reverse("Depth (m)", expand=c(0,0)) +
 opts
 
-
-
-
+#    
+# ------------------
+# Not too bad, the front is (even more) easy to visualize and appears more vertical than when working on salinity data only.
+# Should be tested on other transects.
 
