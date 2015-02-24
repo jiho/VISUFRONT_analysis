79f8d4d8e0ac845870e440073d76ac8786a366cc
Jean-Olivier Irisson <irisson@normalesup.org>
2015-02-24 17:57:35 +0100

    Adapt the interpolation over distance to work with km (not nm)

--- lib_plot.R
+++ lib_plot.R
@@ -40,26 +40,24 @@ interp.time <- function(x, y, z, nx=length(x)/100, y.step=2.5, ...) {
   return(out)
 }
 
-# Interpolate a slice of data for which the x-axis is a distance in nautical miles
+# Interpolate a slice of data for which the x-axis is a distance
 interp.dist <- function(x, y, z, anisotropy=1000, x.step=500, y.step=2.5, smooth=FALSE, theta=0.2, ...) {
   #
   # Interpolate data over a distance coordinate
   #
-  # x   vector of distance *IN NAUTICAL MILES*
+  # x   vector of distance in km
   # y   vector of depth in m
   # z   vector of measured variable
   # anisotropy  anisotropy ratio between x and y
   # x/y.step    interpolation grid steps in m
   # smooth      boolean, wether to smooth the first interpolation using fields::image.smooth
-  # x/y.step.smooth   interpolation grid step for the smoothing
-  # grid.smooth intepolation grid for the smoothing, overrides x/y.step.smooth
   # theta       bandwidth for the kernel smoother in fields::image.smooth
 
   library("akima")
   library("reshape2")
 
   # correct x-axis for anisotropy between horizontal and vertical
-  x <- x * 1852 / anisotropy
+  x <- x / anisotropy
 
   # interpolate
   i <- interp(x=x, y=y, z=z, xo=seq(0, max(x), by=x.step/anisotropy), yo=seq(0, max(y), by=y.step), ...)
@@ -72,7 +70,7 @@ interp.dist <- function(x, y, z, anisotropy=1000, x.step=500, y.step=2.5, smooth
 
   # extract a data.frame
   out <- melt(i$z, varnames=c("x","y"))
-  out$x <- i$x[out$x] * anisotropy / 1852
+  out$x <- i$x[out$x] * anisotropy
   out$y <- i$y[out$y]
 
   return(out)
