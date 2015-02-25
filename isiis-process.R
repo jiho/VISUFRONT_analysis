c583ec041962be1c650955e7f98edf757c042c37
Jean-Olivier Irisson <irisson@normalesup.org>
2015-02-25 14:07:18 +0100

    Remove more erroneous salinity data

--- isiis-process.R
+++ isiis-process.R
@@ -121,8 +121,8 @@ isiis_in_transects <- ddply(transects, ~name, function(x, d) {
     print(ggplot(df) + geom_point(aes(x=dist_from_shore, y=-Depth.m, colour=factor(profile)), na.rm=T) + ggtitle(x$name))
 
     # remove incorrect data
-    # salinity above 30m in downcasts
-    df[which(df$down.up %in% "down" & df$Depth.m <= 30), c("Salinity.PPT", "Density")] <- NA
+    # salinity above 40m in downcasts
+    df[which(df$down.up %in% "down" & df$Depth.m <= 40), c("Salinity.PPT", "Density")] <- NA
     # ggplot(df, aes(x=dist_from_start, y=-Depth.m, colour=Salinity.PPT))  + geom_point() + scale_color_spectral()
     # and irradiance
     df <- select(df, -Irrandiance.UE.cm)
